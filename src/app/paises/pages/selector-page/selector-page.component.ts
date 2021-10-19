b2c62b0ef85a06b04f2b6a54aa6979c9bc825b95
Braulio Talamante <braulionm@hotmail.com>
2021-10-18 21:34:30 -0500

    Selectores

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesServiceService } from '../../services/paises-service.service';
+import { Pais, PaisSmall } from '../../interfaces/paises-interface';
+import { switchMap, tap } from "rxjs/operators";
 
 @Component({
   selector: 'app-selector-page',
@@ -11,16 +13,96 @@ import { PaisesServiceService } from '../../services/paises-service.service';
 export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    region: ['', Validators.required]
+    region: ['', Validators.required],
+    pais: ['', Validators.required],
+    frontera: ['', Validators.required]
   });
 
   // Llenar selectores
   regiones: string[] = [];
+  paises: PaisSmall[] = [];
+  fronteras: string[] = [];
 
   constructor(private fb: FormBuilder, private ps: PaisesServiceService) { }
 
   ngOnInit(): void {
     this.regiones = this.ps.regiones;
+
+    // Cuando cambie la region
+    // observable del control region y que nos ayuda a detectar cuando existen cambios
+    // dentro del combo Continente
+    // y al cambiar traemos los paises de dicha región
+    // el value Changes regresa el observable necesario para esto
+    /**
+     * Podemos refactorizar el código de abajo usando un switchmap
+     */
+    // this.miFormulario.get('region')?.valueChanges
+    //   .subscribe(region => {
+    //     this.ps.getPaisesPorRegion(region)
+    //       .subscribe(paises => {
+    //         this.paises = paises;
+    //       })
+    //   });
+    this.miFormulario.get('region')?.valueChanges
+      .pipe(
+        tap(
+          /**
+           * 
+           * Tap nos ayuda a poder realizar acciones previas a otros operadores
+           */
+          (_) => {
+            this.miFormulario.get('pais')?.reset('');
+          }),
+        switchMap(
+          region => {
+            return this.ps.getPaisesPorRegion(region);
+          }
+        )
+      )
+      .subscribe(paises => {
+        this.paises = paises;
+      });
+
+    this.miFormulario.get('pais')?.valueChanges
+      .pipe(
+        tap(
+          (_) => {
+            this.fronteras = [];
+            this.miFormulario.get('frontera')?.reset('');
+          }
+        ),
+        switchMap(
+          codigo => {
+            return this.ps.getFronteras(codigo);
+          }
+        )
+      )
+      .subscribe(
+        frontera => {
+          /**
+           * Aquí le decimos que si el valor es nulo asigne un arreglo vacío
+           */
+          this.fronteras = frontera?.borders || [];
+          console.log('frontera', frontera);
+        }
+      )
+    // .pipe(
+    //   tap(
+    //     (_) => {
+    //       this.miFormulario.get('frontera')?.reset('');
+    //     }),
+    //   switchMap(
+    //     pais => {
+    //       console.log(pais.code);
+    //       console.log('pais', pais);
+    //       return this.ps.getFronteras(pais);
+    //     }
+    //   )
+    // )
+    // .subscribe(fronteras => {
+    //   // this.pais?.borders = fronteras.borders;
+    //   console.log(fronteras);
+    // });
   }
 
   guardar() {
@@ -28,3 +110,5 @@ export class SelectorPageComponent implements OnInit {
   }
 
 }
+
+
--- src/app/paises/services/paises-service.service.ts
+++ src/app/paises/services/paises-service.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Pais, PaisSmall } from '../interfaces/paises-interface';
 
 @Injectable({
   providedIn: 'root'
@@ -6,10 +9,26 @@ import { Injectable } from '@angular/core';
 export class PaisesServiceService {
 
   private _regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  private _baseUrl: string = 'https://restcountries.com/v2';
 
   get regiones() {
     return [...this._regiones];
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPaisesPorRegion(region: string): Observable<PaisSmall[]> {
+    const url: string = `${this._baseUrl}/region/${region}?fields=alpha3Code,name`
+    return this.http.get<PaisSmall[]>(url);
+  }
+
+  getFronteras(pais: string): Observable<Pais | null> {
+    if (!pais) {
+      return of(null);
+    }
+    const url: string = `${this._baseUrl}/alpha/${pais}`;
+    return this.http.get<Pais>(url);
+
+  }
+
 }
